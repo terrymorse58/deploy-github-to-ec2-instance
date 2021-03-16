73ea4ae936b56d7533a4e2f5052b06f387877786
Terry Morse <tmorse@terrymorse.com>
2021-03-16 09:58:00 -0700

    version 1.0.3

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // sample express app from
 // https://medium.com/@sunilmore/complete-setup-for-deploying-nodejs-app-with-mongodb-database-on-amazon-ec2-e6eeb3b47bc0
 //
-var express = require("express");
-var app = express();
-var MongoClient = require("mongodb").MongoClient;
+const express = require("express");
+const app = express();
+const MongoClient = require("mongodb").MongoClient;
 
 app.get("/", function(req, res) {
   res.send(`<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Hello World</title></head><body>Hello World!</body></html>`);
